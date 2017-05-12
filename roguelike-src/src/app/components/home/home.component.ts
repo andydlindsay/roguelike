c5d3aa39f3362d146538142a7ef80726f06e4563
Andy Lindsay <andydlindsay@gmail.com>
2017-05-12 15:11:54 -0700

    adds ability to move around playarea

--- roguelike-src/src/app/components/home/home.component.ts
+++ roguelike-src/src/app/components/home/home.component.ts
@@ -65,6 +65,8 @@ export class HomeComponent implements OnInit {
     }
     console.log(this.dungeon.nodes[hero.currentNode]);
 
+    const nodes = this.dungeon.nodes;
+
     // append svg component
     // zoom based on Sebastian Gruhier's post
     // https://coderwall.com/p/psogia/simplest-way-to-add-zoom-pan-on-d3-js
@@ -144,30 +146,93 @@ export class HomeComponent implements OnInit {
     // movement in nodes
     d3.select('body')
       .on('keydown', () => {
+
         let destinationNode;
         if (d3.event.keyCode == 37) {
-          console.log('left');
+          // console.log('left');
           destinationNode = hero.currentNode - 1;
         } else if (d3.event.keyCode == 38) {
-          console.log('up');
+          // console.log('up');
           destinationNode = hero.currentNode - this.dungeon.width;
         } else if (d3.event.keyCode == 39) {
-          console.log('right');
+          // console.log('right');
           destinationNode = hero.currentNode + 1;
         } else if (d3.event.keyCode == 40) {
-          console.log('down');
+          // console.log('down');
           destinationNode = hero.currentNode + this.dungeon.width;
         }
-        if (this.dungeon.nodes[destinationNode]['type'] == 'w') {
-          console.log('Ouch! You hit the wall!');
-        } else {
-          console.log('That\'s a valid move!');
-          this.dungeon.nodes[destinationNode]['type'] = 'p';
-          this.dungeon.nodes[hero.currentNode]['type'] = 'f';
-          hero.currentNode = destinationNode;
-          // updateData();
+
+        if (destinationNode != undefined) {
+          if (this.dungeon.nodes[destinationNode]['type'] == 'w') {
+            console.log('Ouch! You hit the wall!');
+          } else {
+            // console.log('That\'s a valid move!');
+            const nodeType = this.dungeon.nodes[destinationNode]['type'];
+            // console.log('nodeType:', nodeType);
+            if (nodeType == 'h') {
+              console.log('That\'s a health pickup.');
+            } else if (nodeType == 'e') {
+              console.log('It\'s go time!');
+            } else if (nodeType == 'b') {
+              console.log('You\'ve found the boss!');
+            } else if (nodeType == 'n') {
+              console.log('Something went seriously wrong.');
+            } else if (nodeType == 'u') {
+              console.log('That\'s a weapon upgrade.');
+            }
+            this.dungeon.nodes[destinationNode]['type'] = 'p';
+            this.dungeon.nodes[hero.currentNode]['type'] = 'f';
+            hero.currentNode = destinationNode;
+            updateData();
+          }
         }
+        
       });
+
+      function updateData() {
+        // console.log('updateData has been called');
+
+        svg.select('.playarea').exit().remove();
+
+        svg.append('g')
+        .attr('class', 'playarea')
+        .selectAll('rect')
+        .filter('.playarea-node')
+        .data(nodes)
+        .enter()
+        .append('rect')
+        .attr('class', (d) => {
+          if (d['type'] == 'h') {
+            return 'playarea-node hero';
+          } else {
+          return 'playarea-node';
+          }
+        })
+        .attr('x', (d) => playareaXZero + (d['col'] * playareaRectWidth))
+        .attr('y', (d) => playareaYZero + (d['row'] * playareaRectHeight))
+        .attr('width', playareaRectWidth)
+        .attr('height', playareaRectHeight)
+        .attr('fill', (d) => {
+          if (d['type'] == 'w') {
+            return '#766951';
+          } else if (d['type'] == 'f') {
+            return '#D7C7AD';
+          } else if (d['type'] == 'p') {
+            return 'blue';
+          } else if (d['type'] == 'e') {
+            return 'red';
+          } else if (d['type'] == 'h') {
+            return 'green';
+          } else if (d['type'] == 'u') {
+            return 'yellow';
+          } else if (d['type'] == 'b') {
+            return 'purple';
+          } else {
+            return 'none';
+          }
+        });
+
+      }
 
   }
 
