ef9ee8803a1d026d3786ad1f736798e7f5e5c6f0
Andy Lindsay <andydlindsay@gmail.com>
2017-05-12 01:19:56 -0700

    adds ability to draw playarea

--- roguelike-src/src/app/components/home/home.component.ts
+++ roguelike-src/src/app/components/home/home.component.ts
@@ -14,8 +14,7 @@ import * as topojson from 'topojson';
 export class HomeComponent implements OnInit {
 
   private d3: D3;
-  dataset: any;
-  meteors: any;
+  dungeon: any;
 
   constructor(
     d3Service: D3Service,
@@ -29,23 +28,20 @@ export class HomeComponent implements OnInit {
     // title the page
     this.titleService.setTitle('Roguelike - FCC');
 
-    this.data.getJson().subscribe(
-      data => {
-        if (data) {
-          this.dataset = data;
-          // console.log(data);
-          this.data.getMeteor().subscribe(
-            data => {
-              if (data) {
-                this.meteors = data.features;
-                // console.log(data);
-                this.drawRoguelike();
-              }
-            }
-          );
-        }
-      }
-    );
+    let okay = true;
+
+    // retrieve gameboard
+    this.dungeon = this.data.getDungeon();
+    // confirm that dungeon is valid
+    if (this.dungeon.nodes.length != this.dungeon.width * this.dungeon.height) {
+      console.error('Dungeon level is not valid.');
+      okay = false;
+    }
+
+    // draw
+    if (okay) {
+      this.drawRoguelike();
+    }
 
   }
 
@@ -56,7 +52,18 @@ export class HomeComponent implements OnInit {
     // setup svg component
     const width = 1312,
           height = 756,
-          padding = 50;
+          padding = 60,
+          internalPadding = 20;
+
+    // hero object
+    let hero = {
+      'health': 20,
+      'level': 1,
+      'weapon': 'Dagger',
+      'maxDamage': 4,
+      'currentNode': 70
+    }
+    console.log(this.dungeon.nodes[hero.currentNode]);
 
     // append svg component
     // zoom based on Sebastian Gruhier's post
@@ -77,9 +84,90 @@ export class HomeComponent implements OnInit {
       .attr('class', 'tooltip')
       .style('opacity', 0);
 
-    
+    // add row and column info to this.dungeon
+    for (let i = 0; i < this.dungeon.height; i++) {
+      for (let j = 0; j < this.dungeon.width; j++) {
+        this.dungeon.nodes[(i * this.dungeon.width) + j].row = i;
+        this.dungeon.nodes[(i * this.dungeon.width) + j].col = j;
+      }
+    }
+    console.log('dungeon:', this.dungeon);
 
+    // positions of play area
+    const playareaWidth = Math.round(width / 2 - (2 * internalPadding));
+    const playareaHeight = Math.round(height * 0.8 - (2 * internalPadding));
+    const playareaXZero = 0 + internalPadding;
+    const playareaYZero = height - playareaHeight - (internalPadding * 2);
 
+    // size of play area rects
+    const playareaRectWidth = playareaWidth / this.dungeon.width;
+    const playareaRectHeight = playareaHeight / this.dungeon.height;
+
+    svg.append('g')
+      .attr('class', 'playarea')
+      .selectAll('rect')
+      .filter('.playarea-node')
+      .data(this.dungeon.nodes)
+      .enter()
+      .append('rect')
+      .attr('class', (d) => {
+        if (d['type'] == 'h') {
+          return 'playarea-node hero';
+        } else {
+        return 'playarea-node';
+        }
+      })
+      .attr('x', (d) => playareaXZero + (d['col'] * playareaRectWidth))
+      .attr('y', (d) => playareaYZero + (d['row'] * playareaRectHeight))
+      .attr('width', playareaRectWidth)
+      .attr('height', playareaRectHeight)
+      .attr('fill', (d) => {
+        if (d['type'] == 'w') {
+          return '#766951';
+        } else if (d['type'] == 'f') {
+          return '#D7C7AD';
+        } else if (d['type'] == 'p') {
+          return 'blue';
+        } else if (d['type'] == 'e') {
+          return 'red';
+        } else if (d['type'] == 'h') {
+          return 'green';
+        } else if (d['type'] == 'u') {
+          return 'yellow';
+        } else if (d['type'] == 'b') {
+          return 'purple';
+        } else {
+          return 'none';
+        }
+      });
+
+    // movement in nodes
+    d3.select('body')
+      .on('keydown', () => {
+        let destinationNode;
+        if (d3.event.keyCode == 37) {
+          console.log('left');
+          destinationNode = hero.currentNode - 1;
+        } else if (d3.event.keyCode == 38) {
+          console.log('up');
+          destinationNode = hero.currentNode - this.dungeon.width;
+        } else if (d3.event.keyCode == 39) {
+          console.log('right');
+          destinationNode = hero.currentNode + 1;
+        } else if (d3.event.keyCode == 40) {
+          console.log('down');
+          destinationNode = hero.currentNode + this.dungeon.width;
+        }
+        if (this.dungeon.nodes[destinationNode]['type'] == 'w') {
+          console.log('Ouch! You hit the wall!');
+        } else {
+          console.log('That\'s a valid move!');
+          this.dungeon.nodes[destinationNode]['type'] = 'p';
+          this.dungeon.nodes[hero.currentNode]['type'] = 'f';
+          hero.currentNode = destinationNode;
+          // updateData();
+        }
+      });
 
   }
 
